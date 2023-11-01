6ecd06ef5eed62e432864b1290676041ae9d4e3c
halla <shulihalla@gmail.com>
2023-11-01 14:13:09 +0200

    complete sing up

--- app/src/main/java/shu/example/hallafinal2023/Singin.java
+++ app/src/main/java/shu/example/hallafinal2023/Singin.java
@@ -13,7 +13,8 @@ public class Singin extends AppCompatActivity {
     private Button btnSingup;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singin);
     }
---
+++ app/src/main/java/shu/example/hallafinal2023/singup.java
@@ -0,0 +1,13 @@
+package shu.example.hallafinal2023;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class singup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+    }
+}

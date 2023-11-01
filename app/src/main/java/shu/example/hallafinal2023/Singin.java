0b7cf1fdd2f4ad1c0ff889aa3236f0961dc463b7
halla <shulihalla@gmail.com>
2023-11-01 13:44:42 +0200

    complete sing in and sing up

--- app/src/main/java/shu/example/hallafinal2023/Singin.java
+++ app/src/main/java/shu/example/hallafinal2023/Singin.java
@@ -1,10 +1,16 @@
 package shu.example.hallafinal2023;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Singin extends AppCompatActivity {
+    private TextInputLayout etUsername;
+    private TextInputLayout etPassword;
+    private Button btnSingin;
+    private Button btnSingup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

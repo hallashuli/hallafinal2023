a898c13f7ce5ecbc151ba3b46d6b308a1812840e
halla <shulihalla@gmail.com>
2023-11-19 09:24:00 +0200

    اكمال

---
+++ app/src/main/java/shu/example/hallafinal2023/Data/Data/Myuser/UserQuaery.java
@@ -0,0 +1,46 @@
+package shu.example.hallafinal2023.Data.Data.Myuser;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+/**
+ * الواجهة تحتوي استعلامات على قاعدة البيانات
+ */
+
+@Dao
+public interface UserQuaery {
+    @Query("SELECT * FROM User")
+    List<User> getAll();
+
+    @Query("SELECT * FROM User WHERE keyid IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM User WHERE email = :myEmail AND " +
+            "passw = :myPassw LIMIT 1")
+    User checkEmailPassw(String myEmail, String myPassw);
+
+    @Query("SELECT * FROM User WHERE email = :myEmail ")
+    User checkEmail(String myEmail);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+
+    @Query("Delete From User WHERE keyid=:id ")
+    void delete(int id);
+
+    @Insert
+    void insert(User myUser);
+    @Update
+    void update(User...values);
+}
+
+
+
--- app/src/main/java/shu/example/hallafinal2023/Data/Data/Myuser/UserQuary.java
+++
@@ -1,4 +0,0 @@
-package shu.example.hallafinal2023.Data.Data.Myuser;
-
-public enum UserQuary {
-}

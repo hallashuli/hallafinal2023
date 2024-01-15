09bb91a2ded7b51adffc994525bdb11643d26863
halla <shulihalla@gmail.com>
2024-01-15 09:44:16 +0200

    complete...

--- app/src/main/java/shu/example/hallafinal2023/MainActivityMovei.java
+++ app/src/main/java/shu/example/hallafinal2023/MainActivityMovei.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Spinner;
@@ -14,7 +13,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivityMovei extends AppCompatActivity {
     private FloatingActionButton fabAdd;
-    private Button btnM;
     private SearchView srchv;
     private Spinner spnr;
     private ListView istTv;
@@ -24,7 +22,6 @@ public class MainActivityMovei extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_movei);
-        btnM=findViewById(R.id.btnM);
         spnr=findViewById(R.id.spnrM);
         istTv=findViewById(R.id.istTvM);
         fabAdd=findViewById(R.id.fabAdd);
--- app/src/main/java/shu/example/hallafinal2023/MainActivitySeries.java
+++ app/src/main/java/shu/example/hallafinal2023/MainActivitySeries.java
@@ -3,12 +3,26 @@ package shu.example.hallafinal2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Spinner;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivitySeries extends AppCompatActivity {
+    private FloatingActionButton faBaDd;
+    private SearchView srchS;
+    private Spinner spnrS;
+    private ListView istvS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_series);
+        spnrS=findViewById(R.id.spnrS);
+        istvS=findViewById(R.id.istvS);
+        faBaDd=findViewById(R.id.faBaDd);
+        faBaDd.setOnClickListener(new View.OnClickListener ()
     }
 }

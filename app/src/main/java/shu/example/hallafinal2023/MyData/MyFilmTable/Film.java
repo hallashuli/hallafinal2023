9570ed4e94dac8134a20fab1d253a6f5433c7d35
halla <shulihalla@gmail.com>
2024-01-21 23:10:54 +0200

    complete... add series... table film...

---
+++ app/src/main/java/shu/example/hallafinal2023/MyData/MyFilmTable/Film.java
@@ -0,0 +1,12 @@
+package shu.example.hallafinal2023.MyData.MyFilmTable;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Film {
+    @PrimaryKey(autoGenerate = true)
+    //**
+
+
+}

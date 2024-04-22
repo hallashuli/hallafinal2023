fb482866b77e337e2389e0afda374e6f86beaa43
halla <shulihalla@gmail.com>
2024-04-22 11:49:07 +0300

    complete...Fire Bace on add series

--- app/src/main/java/shu/example/hallafinal2023/AddSeries.java
+++ app/src/main/java/shu/example/hallafinal2023/AddSeries.java
@@ -1,5 +1,4 @@
 package shu.example.hallafinal2023;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,18 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import shu.example.hallafinal2023.MyData.MySeries.Series;
 
 public class AddSeries extends AppCompatActivity {
     private TextInputEditText name2;
-    private TextInputEditText Type2;
+    private TextInputEditText type2;
     private TextInputEditText lang2;
     private TextInputEditText num2;
     private TextInputEditText time2;
@@ -31,7 +28,7 @@ public class AddSeries extends AppCompatActivity {
         setContentView(R.layout.activity_add_series);
             //توقيت الصفات
             name2=findViewById(R.id.name2);
-            Type2=findViewById(R.id.type2);
+            type2=findViewById(R.id.type2);
             lang2=findViewById(R.id.lang2);
             num2=findViewById(R.id.num2);
            time2=findViewById(R.id.time2);
@@ -41,51 +38,51 @@ public class AddSeries extends AppCompatActivity {
         Intent i = new Intent(AddSeries.this, Reating.class);
         startActivity(i);
     }
-    private void cheackSeriesDetails(){
+    private void cheackSeriesDetails() {
         boolean isAllok = true; // يحوي نتيجة فحص الحقول ان كانت  السليمة
         //يستخرج اسم المسلسل
-        String Name= name2.getText().toString();
+        String Name = name2.getText().toString();
         //يستخرج نوع المسلسل
-        String Type= Type2.getText().toString();
+        String Type = type2.getText().toString();
         //يستخرج لغة المسلسل
-        String Langage= lang2.getText().toString();
+        String Langage = lang2.getText().toString();
         //يستخرد عدد حلقات المسلسل
-        String EpisodeNumber= num2.getText().toString();
+        String EpisodeNumber = num2.getText().toString();
         //يستخرج مدة المسلسل
-        String Time= time2.getText().toString();
-        if (name2.length() < 0){
+        String Time = time2.getText().toString();
+        if (name2.length() < 0) {
             // تعديل المتغير و يدل على انه فحص و يعطي نتيجة خاطئة
             isAllok = false;
             //عرض النتيجة خطأ في حقل
             name2.setError("worng name");
         }
-        if (Type2.length() < 0){
+        if (type2.length() < 0) {
             // تعديل المتغير و يدل على انه فحص و يعطي نتيجة خاطئة
             isAllok = false;
             //عرض النتيجة خطأ في حقل
-            Type2.setError("worng Type");
+            type2.setError("worng Type");
         }
-        if (lang2.length() < 0){
+        if (lang2.length() < 0) {
             // تعديل المتغير و يدل على انه فحص و يعطي نتيجة خاطئة
             isAllok = false;
             //عرض النتيجة خطأ في حقل
-           lang2.setError("worng langage");
+            lang2.setError("worng langage");
         }
-        if (num2.length() < 0){
+        if (num2.length() < 0) {
             // تعديل المتغير و يدل على انه فحص و يعطي نتيجة خاطئة
             isAllok = false;
             //عرض النتيجة خطأ في حقل
             num2.setError("worng Epissode Number");
         }
-        if (time2.length() < 0){
+        if (time2.length() < 0) {
             // تعديل المتغير و يدل على انه فحص و يعطي نتيجة خاطئة
             isAllok = false;
             //عرض النتيجة خطأ في حقل
             time2.setError("worng Time");
         }
-        if (isAllok) {
-            saveSeries_FB(Name,Type,Langage,EpisodeNumber,Time);
+        if (isAllok) {saveSeries_FB(Name, Type, Langage, EpisodeNumber, Time);
         }
+    }
         //Firebase
         private void saveSeries_FB(String name2, String Type2, String lang2, String num2,String time2 )
         {
@@ -96,8 +93,8 @@ public class AddSeries extends AppCompatActivity {
             //بناء الكائن الذي سيتم حفظه
             ٍSeries series=new Series();
             series.setSeriesName(name2);
-            series.setSeriesLangage();Langage(lang2);
-            series.setSeriesType(Type2);
+            series.setSeriesLangage(lang2);
+            series.setSeriesType(type2);
             series.setSeriesEpisodeNumber(num2);
             series.setSeriesTime(time2);
             //اضافة كائن لمجموعة المستعملين ومعالج حدث لفحص نجاح الاضافة
@@ -115,4 +112,3 @@ public class AddSeries extends AppCompatActivity {
 
         }
     }
-}
